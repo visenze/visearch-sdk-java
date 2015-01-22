7bae2b9312770b88f0a55787532f6ef0332edd3d
jasonpeng <jasonpeng.boy@gmail.com>
2015-01-22 18:43:52 +0800

    Added upload search using default resize settings

--- visearch-java/src/main/java/com/visenze/visearch/internal/SearchOperations.java
+++ visearch-java/src/main/java/com/visenze/visearch/internal/SearchOperations.java
@@ -8,6 +8,8 @@ public interface SearchOperations {
 
     PagedSearchResult<ImageResult> colorSearch(ColorSearchParams colorSearchParams);
 
+    PagedSearchResult<ImageResult> uploadSearch(UploadSearchParams uploadSearchParams);
+
     PagedSearchResult<ImageResult> uploadSearch(UploadSearchParams uploadSearchParams, ResizeSettings resizeSettings);
 
 }
