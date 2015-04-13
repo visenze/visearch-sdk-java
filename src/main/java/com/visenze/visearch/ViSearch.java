5069d380df9b26b31667f146ed8a83c3970fe2bc
jasonpeng <jasonpeng.boy@gmail.com>
2015-04-13 15:58:15 +0800

    Update docs for main interfaces

--- src/main/java/com/visenze/visearch/ViSearch.java
+++ src/main/java/com/visenze/visearch/ViSearch.java
@@ -12,25 +12,29 @@ import com.visenze.visearch.internal.json.ViSearchModule;
 import java.net.URL;
 import java.util.List;
 
-/**
- * ViSearch APIs.
- *
- * See online documentation for <a href="http://www.visenze.com/doc/api/data">Data API</a>
- * and <a href="http://www.visenze.com/doc/api/search">Search API</a>.
- *
- * viSearch.insert(...)
- * viSearch.remove(...)
- * viSearch.search(...)
- * viSearch.colorSearch(...)
- * viSearch.uploadSearch(...)
- */
+
 public class ViSearch implements DataOperations, SearchOperations {
 
+    /**
+     * Default ViSearch API base endpoint.
+     */
     private static final String API_ENDPOINT = "http://visearch.visenze.com";
 
+    /**
+     * ViSearch Data API i.e. /insert, /insert/status, /remove.
+     */
     private final DataOperations dataOperations;
+
+    /**
+     * ViSearch Search API i.e. /search, /uploadsearch, /colorsearch.
+     */
     private final SearchOperations searchOperations;
 
+    /**
+     * Construct a ViSearch client to call the default endpoint with access key and secret key.
+     * @param accessKey ViSearch App access key
+     * @param secretKey ViSearch App secret key
+     */
     public ViSearch(String accessKey, String secretKey) {
         ViSearchHttpClient viSearchHttpClient = new ViSearchHttpClientImpl(API_ENDPOINT, accessKey, secretKey);
         ObjectMapper objectMapper = new ObjectMapper().registerModule(new ViSearchModule());
@@ -38,6 +42,12 @@ public class ViSearch implements DataOperations, SearchOperations {
         this.searchOperations = new SearchOperationsImpl(viSearchHttpClient, objectMapper);
     }
 
+    /**
+     * (For testing) Construct a ViSearch client to call a custom endpoint with access key and secret key.
+     * @param endpoint custom ViSearch endpoint
+     * @param accessKey ViSearch App access key
+     * @param secretKey ViSearch App secret key
+     */
     public ViSearch(String endpoint, String accessKey, String secretKey) {
         if (endpoint == null) {
             throw new ViSearchException("Endpoint is not specified");
@@ -48,6 +58,12 @@ public class ViSearch implements DataOperations, SearchOperations {
         this.searchOperations = new SearchOperationsImpl(viSearchHttpClient, objectMapper);
     }
 
+    /**
+     * (For testing) Construct a ViSearch client to call a custom endpoint with access key and secret key.
+     * @param endpoint custom ViSearch endpoint
+     * @param accessKey ViSearch App access key
+     * @param secretKey ViSearch App secret key
+     */
     public ViSearch(URL endpoint, String accessKey, String secretKey) {
         if (endpoint == null) {
             throw new ViSearchException("Endpoint is not specified");
@@ -58,41 +74,82 @@ public class ViSearch implements DataOperations, SearchOperations {
         this.searchOperations = new SearchOperationsImpl(viSearchHttpClient, objectMapper);
     }
 
+    /**
+     * Insert images to the ViSearch App.
+     * @param imageList the list of Images to insert.
+     * @return an insert transaction
+     */
     @Override
     public InsertTransaction insert(List<Image> imageList) {
         return dataOperations.insert(imageList);
     }
 
+    /**
+     * Get recent insert transactions in pages.
+     * @param page the page number
+     * @param limit the number of item returning per page.
+     * @return the page of insert transactions
+     */
     @Override
     public PagedResult<InsertTransaction> getStatus(Integer page, Integer limit) {
         return dataOperations.getStatus(page, limit);
     }
 
+    /**
+     * Get a single insert transaction by transaction id.
+     * @param transactionId the id of the insert transaction.
+     * @return the insert transaction
+     */
     @Override
     public InsertTransaction getStatus(String transactionId) {
         return dataOperations.getStatus(transactionId);
     }
 
+    /**
+     * Remove a list of images from the ViSearch App, identified by their im_names.
+     * @param imNameList the list of im_names of the images to be removed
+     */
     @Override
     public void remove(List<String> imNameList) {
         dataOperations.remove(imNameList);
     }
 
+    /**
+     * Search for similar images from the ViSearch App given an existing image in the App.
+     * @param searchParams the search parameters, must contain the im_name of the existing image
+     * @return the page of search result
+     */
     @Override
     public PagedSearchResult search(SearchParams searchParams) {
         return searchOperations.search(searchParams);
     }
 
+    /**
+     * Search for similar images from the ViSearch App given a hex color.
+     * @param colorSearchParams the color search parameters, must contain the hex color
+     * @return the page of color search result
+     */
     @Override
     public PagedSearchResult colorSearch(ColorSearchParams colorSearchParams) {
         return searchOperations.colorSearch(colorSearchParams);
     }
 
+    /**
+     * Search for similar images from the ViSearch App given an image file or url.
+     * @param uploadSearchParams the upload search parameters, must contain a image file or a url
+     * @return the page of upload search result
+     */
     @Override
     public PagedSearchResult uploadSearch(UploadSearchParams uploadSearchParams) {
         return searchOperations.uploadSearch(uploadSearchParams);
     }
 
+    /**
+     * Search for similar images from the ViSearch App given an image file or url, with customized image resize option.
+     * @param uploadSearchParams the upload search parameters, must contain a image file or a url
+     * @param resizeSettings custom image resize option
+     * @return the page of upload search result
+     */
     @Override
     public PagedSearchResult uploadSearch(UploadSearchParams uploadSearchParams, ResizeSettings resizeSettings) {
         return searchOperations.uploadSearch(uploadSearchParams, resizeSettings);
