2446f0785bca795dfcf6da1c31149e82c49e75e0
jasonpeng <jasonpeng.boy@gmail.com>
2015-05-11 21:33:05 +0800

    Add ignor unknown flag to InsertErrorMixin

--- src/main/java/com/visenze/visearch/internal/json/InsertErrorMixin.java
+++ src/main/java/com/visenze/visearch/internal/json/InsertErrorMixin.java
@@ -1,8 +1,9 @@
 package com.visenze.visearch.internal.json;
 
-
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 abstract class InsertErrorMixin {
 
     protected String imName;
