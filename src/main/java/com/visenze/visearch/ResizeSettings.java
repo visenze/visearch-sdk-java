09dbe4839eeee612933f5c4a9a519cfd67296d7b
jasonpeng <jasonpeng.boy@gmail.com>
2015-05-14 19:34:49 +0800

    Use delta for float comparison in ResizeSettings

--- src/main/java/com/visenze/visearch/ResizeSettings.java
+++ src/main/java/com/visenze/visearch/ResizeSettings.java
@@ -7,6 +7,8 @@ public class ResizeSettings {
 
     public static ResizeSettings HIGH = new ResizeSettings(1024, 1024, 0.985f);
 
+    public static float DELTA = 0.000001f;
+
     private int width;
     private int height;
     private float quality;
@@ -14,9 +16,9 @@ public class ResizeSettings {
     public ResizeSettings(int width, int height, float quality) {
         this.width = width;
         this.height = height;
-        if (quality < 0) {
+        if (Math.abs(quality) < DELTA) {
             this.quality = 0;
-        } else if (quality > 1.0f) {
+        } else if (quality + DELTA > 1.0f) {
             this.quality = 1.0f;
         } else {
             this.quality = quality;
