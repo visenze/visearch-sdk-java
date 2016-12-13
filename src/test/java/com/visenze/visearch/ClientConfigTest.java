e5c3208afd0312f7ee213bc58ee69f2722d68bcf
houdejun214 <houdejun214@gmail.com>
2016-12-13 16:52:45 +0800

    Increase version

--- src/test/java/com/visenze/visearch/ClientConfigTest.java
+++ src/test/java/com/visenze/visearch/ClientConfigTest.java
@@ -12,12 +12,14 @@ public class ClientConfigTest {
     public void getDefaultUserAgent() throws Exception {
         String version = ViSearch.VISEACH_JAVA_SDK_VERSION;
         assertTrue(ClientConfig.DEFAULT_USER_AGENT.startsWith("visearch-sdk-java/"+version));
+        System.out.println(ClientConfig.DEFAULT_USER_AGENT);
     }
 
     @Test
     public void getDefaultXRequestWith() throws Exception {
         String version = ViSearch.VISEACH_JAVA_SDK_VERSION;
         assertEquals("visearch-sdk-java/"+version, ClientConfig.DEFAULT_XREQUEST_WITH);
+        System.out.println(ClientConfig.DEFAULT_XREQUEST_WITH);
     }
 
 }
