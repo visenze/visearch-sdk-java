2d7b0d1949403a5cce39c893da5f047fb596070b
David Huang <david.huang@visenze.com>
2021-06-17 15:19:23 +0800

    Fix unit test cases for changed types

--- src/test/java/com/visenze/visearch/RecommendSearchParamsTest.java
+++ src/test/java/com/visenze/visearch/RecommendSearchParamsTest.java
@@ -17,7 +17,7 @@ public class RecommendSearchParamsTest {
         RecommendSearchParams recommendSearchParams = new RecommendSearchParams("im_name");
         recommendSearchParams.setRecommendationAlgorithm("STL");
         recommendSearchParams.setAltLimit(10);
-        recommendSearchParams.setDedupBy(Lists.newArrayList("parent_id"));
+        recommendSearchParams.setDedupBy("parent_id");
         Multimap<String, String> params = recommendSearchParams.toMap();
         assertArrayEquals(new String[]{"im_name"}, params.get("im_name").toArray(new String[0]));
         assertArrayEquals(new String[]{"STL"}, params.get("recommendation_algorithm").toArray(new String[0]));
@@ -31,7 +31,7 @@ public class RecommendSearchParamsTest {
         Multimap<String, String> params = recommendSearchParams.toMap();
         assertArrayEquals(new String[]{"im_name"}, params.get("im_name").toArray(new String[0]));
         assertArrayEquals(new String[]{}, params.get("recommendation_algorithm").toArray(new String[0]));
-        assertArrayEquals(new String[]{"5"}, params.get("alt_limit").toArray(new String[0]));
+        assertArrayEquals(new String[]{}, params.get("alt_limit").toArray(new String[0]));
         assertArrayEquals(new String[]{}, params.get("dedup_by").toArray(new String[0]));
     }
 }
